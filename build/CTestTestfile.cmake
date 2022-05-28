196abc8de278ac45b585ca9291524a14024e6861
Spencer <spencer.huanhuan@outlook.com>
2022-05-28 01:21:20 -0700

    test

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/spencer/work_spaces/x1918_ur5/src
+# Build directory: /home/spencer/work_spaces/x1918_ur5/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/spencer/work_spaces/x1918_ur5/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/spencer/work_spaces/x1918_ur5/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/spencer/work_spaces/x1918_ur5/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/spencer/work_spaces/x1918_ur5/src:$ROS_PACKAGE_PATH"
