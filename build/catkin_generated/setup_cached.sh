5334015ab6844f4816c16688b9a808bb3822bab9
Spencer <spencer.huanhuan@outlook.com>
2022-05-28 02:13:37 -0700

    v1

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/spencer/work_spaces/x1918_ur5/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/spencer/work_spaces/x1918_ur5/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/spencer/work_spaces/x1918_ur5/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/spencer/work_spaces/x1918_ur5/build'
+export PYTHONPATH="/home/spencer/work_spaces/x1918_ur5/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/spencer/work_spaces/x1918_ur5/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/spencer/work_spaces/x1918_ur5/src:$ROS_PACKAGE_PATH"
